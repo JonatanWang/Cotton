ce1655696f8e6b46889471aee0dbee59daafd590
jonathan <jonte.k_1993@hotmail.com>
2016-04-11 14:21:51 +0200

    folder structure for maven integration

--- src/test/java/cotton/TestMain.java
+++ src/test/java/cotton/TestMain.java
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class TestMain 
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public TestMain ( String testName )
     {
         super( testName );
     }
