c7da8fd161c43ce25fcde5f7a14dd73e8891b6a6
magnus-1 <magnus-1@users.noreply.github.com>
2016-04-11 13:42:18 +0200

    Cloudcontext

--- src/mockup/ServiceInstance.java
+++ src/mockup/ServiceInstance.java
@@ -6,5 +6,5 @@ import java.io.Serializable;
  * Created by o_0 on 2016-04-07.
  */
 public interface ServiceInstance {
-    public Serializable consumeServiceOrder(ServiceConnection from, InputStream data,ServiceChain to);
+    public Serializable consumeServiceOrder(CloudContext ctx,ServiceConnection from, InputStream data,ServiceChain to);
 }
