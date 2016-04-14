c2d105b47f996f6002ff5b1ae5b3fb92f89c14f0
gunnlaugur <gju@kth.se>
2016-04-14 10:14:50 +0200

    Test file for DefaultActiveServiceLookup
    Test file for DefaultActiveServiceLookup.

--- src/test/java/cotton/TestDASL.java
+++ src/test/java/cotton/TestDASL.java
@@ -1,8 +1,11 @@
 package test.java.cotton;
 
+import java.util.Enumeration;
+import java.util.concurrent.ConcurrentHashMap;
 import main.java.cotton.mockup.DefaultActiveServiceLookup;
 import main.java.cotton.mockup.ServiceFactory;
 import main.java.cotton.mockup.ServiceInstance;
+import main.java.cotton.mockup.ServiceMetaData;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -49,12 +52,64 @@ public class TestDASL {
     }
     
     @Test
-    public void testRegistrateService() {
+    public void testCapacity() {
         DefaultActiveServiceLookup dasl = new DefaultActiveServiceLookup();
-        dasl.registrateService("Coloring", new TestServiceFactory(), 10);
-        int a = 10, b = 10;
+        dasl.registerService("Coloring", new TestServiceFactory(), 10);
         
-        //assertEquals(10, dasl.getService("Coloring").getMaxCapacity());
-        assertEquals(a, b);
+        assertEquals(10, dasl.getService("Coloring").getMaxCapacity());
+    }
+    
+    @Test
+    public void testWrongCapacity() {
+        DefaultActiveServiceLookup dasl = new DefaultActiveServiceLookup();
+        dasl.registerService("Coloring", new TestServiceFactory(), 10);
+        
+        assertNotEquals(9, dasl.getService("Coloring").getMaxCapacity());
+    }
+    
+    @Test
+    public void testHashMapKeys() {
+        DefaultActiveServiceLookup dasl = new DefaultActiveServiceLookup();
+        
+        String[] services = new String[3];
+        services[0] = "Coloring";
+        services[1] = "Multiplying";
+        services[2] = "Yellow Filtering";
+        
+        for(int i = 0; i < services.length; i++)
+            dasl.registerService(services[i], new TestServiceFactory(), 10);
+        
+        Enumeration<String> hashMapKeys = dasl.getServiceEnumeration();
+        
+        // Checks for "Multiplying" in hashMapKeys
+        String temp = null;
+        while(hashMapKeys.hasMoreElements()) {
+            temp = hashMapKeys.nextElement();
+            if(temp.equals("Multiplying"))
+                break;
+        }
+
+        assertEquals("Multiplying", temp);
+    }
+    
+    @Test
+    public void testRemove() {
+        DefaultActiveServiceLookup dasl = new DefaultActiveServiceLookup();
+        
+        String[] services = new String[3];
+        services[0] = "Coloring";
+        services[1] = "Multiplying";
+        services[2] = "Yellow Filtering";
+        
+        for(int i = 0; i < services.length; i++)
+            dasl.registerService(services[i], new TestServiceFactory(), 10);
+        
+        System.out.println(dasl.removeServiceEntry("Coloring"));
+        
+        boolean removeCheck = false;
+        if(dasl.getService("Coloring") == null)
+            removeCheck = true;
+        
+        assertEquals(true, removeCheck);
     }
 }
