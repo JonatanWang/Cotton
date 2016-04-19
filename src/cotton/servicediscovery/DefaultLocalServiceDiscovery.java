2a386147119224fe51713f30904fb73e6264f0cd
mats <mats@bajs.bajs>
2016-04-19 15:29:05 +0200

    Service Discovery, Local reachable services

--- src/cotton/servicediscovery/DefaultLocalServiceDiscovery.java
+++ src/cotton/servicediscovery/DefaultLocalServiceDiscovery.java
@@ -6,6 +6,10 @@ import cotton.network.ServiceChain;
 import cotton.services.ActiveServiceLookup;
 import cotton.services.ServiceConnection;
 import java.io.InputStream;
+import java.net.InetAddress;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
+import cotton.services.ServiceMetaData;
 
 /**
  *
@@ -14,18 +18,78 @@ import java.io.InputStream;
 public class DefaultLocalServiceDiscovery implements LocalServiceDiscovery {
     private ActiveServiceLookup internalLockup;
     private NetworkHandler network = null;
-    
-    public DefaultLocalServiceDiscovery(ActiveServiceLookup internalLockup) {
-        this.internalLockup = internalLockup;
+    private InetAddress localAddress;
+    private ConcurrentHashMap<String, AddressPool> serviceCache;
+
+    private class AddressPool{
+
+        private int pos = 0;
+        private ArrayList<InetAddress> pool= new ArrayList<InetAddress>();
+
+        public boolean addAddress(InetAddress address){
+
+            synchronized(this){
+                pool.add(address);
+            }
+            return true;
+        }
+
+        public InetAddress getAddress(){
+            InetAddress addr = null;
+            synchronized(this){
+                pos = pos % pool.size();
+
+                if(pool.isEmpty() == false){
+
+                    addr = pool.get(pos);
+                    pos++;
+                }
+            }
+
+            return addr;
+        }
+
     }
 
-    public void setNetwork(NetworkHandler network) {
-        this.network = network;
+    public DefaultLocalServiceDiscovery(ActiveServiceLookup internalLockup) {
+        this.internalLockup = internalLockup;
+        this.serviceCache = new ConcurrentHashMap<String, AddressPool>();
     }
-    
+
+    public void setNetwork(NetworkHandler network, InetAddress localAddress) {
+        this.network = network;
+        this.localAddress = localAddress;
+    }
+
     @Override
     public RouteSignal getDestination(ServiceConnection destination, ServiceConnection from, ServiceChain to) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        String serviceName;
+
+        serviceName = to.getCurrentServiceName();
+
+        if(serviceName == null){
+            if(from == null){
+                return RouteSignal.NOTFOUND;
+            }
+            destination.setAddress(from.getAddress());
+
+            return (from.getAddress().equals(localAddress)) ? RouteSignal.LOCALDESTINATION : RouteSignal.NETWORKDESTINATION;
+        }
+
+        ServiceMetaData serviceInfo = internalLockup.getService(serviceName);
+        if(serviceInfo != null){
+            return RouteSignal.LOCALDESTINATION;
+        }
+        AddressPool pool  = serviceCache.get(serviceName);
+        if(pool == null) {
+            // get global sd
+            return RouteSignal.NOTFOUND;
+        }
+        destination.setAddress(pool.getAddress());
+
+        return RouteSignal.NETWORKDESTINATION;
+
     }
 
     @Override
@@ -37,5 +101,5 @@ public class DefaultLocalServiceDiscovery implements LocalServiceDiscovery {
     public void sendUpdate(ServiceConnection from, InputStream data) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
