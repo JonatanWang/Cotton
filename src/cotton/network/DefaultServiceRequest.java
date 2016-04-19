8ef25e51480ff028bda6ad4a95504d7181937ea9
ksuan <ttran@kth.se>
2016-04-19 17:54:45 +0200

    added files

---
+++ src/cotton/network/DefaultServiceRequest.java
@@ -0,0 +1,36 @@
+package cotton.network;
+
+import java.io.Serializable;
+import java.util.concurrent.CountDownLatch;
+
+
+/**
+ *
+ * @author Tony
+ * @author Jonathan
+ * @author Gunnlaugur
+ * @author Magnus
+ **/
+public class DefaultServiceRequest implements ServiceRequest{
+    private Serializable data = null;
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    @Override
+    public Serializable getData() {
+        boolean loop = false;
+        do {
+            try {
+                latch.await();
+                loop = false;
+            } catch (InterruptedException ex) {loop = true;}
+        }while(loop);
+        return data;
+    }
+
+    public void setData(Serializable data) {
+        this.data = data;
+        latch.countDown();
+    }
+        
+}
+    
---
+++ src/cotton/network/ServiceRequest.java
@@ -0,0 +1,12 @@
+import java.io.Serializable;
+
+
+/**
+ *
+ * @author Tony
+ * @author Jonathan
+ * @author Gunnlaugur
+ **/
+public interface ServiceRequest{
+    public Serializable getData();
+}
