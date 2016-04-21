eca046e42da6a6f650e9e3151efb45df9ab3891d
mats <MatsLevi@users.noreply.github.com>
2016-04-21 16:08:31 +0200

    recive updates from Service Discovery

--- src/cotton/servicediscovery/DiscoveryPacket.java
+++ src/cotton/servicediscovery/DiscoveryPacket.java
@@ -11,8 +11,18 @@ public class DiscoveryPacket implements Serializable {
     public enum DiscoveryPacketType {
         DISCOVERYPROBE,ANNOUNCE
     }
+    private DiscoveryPacketType type;
+
+    public DiscoveryPacketType  getPacketType() {
+        return type;
+    }
+
+    public void setPackettype(DiscoveryPacketType type){
+        this.type = type;
+    }
 
     public DiscoveryPacket(DiscoveryPacketType type) {
+        this.type = type;
     }
     
     private DiscoveryProbe probe = null;
