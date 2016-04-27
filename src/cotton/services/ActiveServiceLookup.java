ee3ad3c58d2b32a271b494c1b5e6ba2ea259c773
gunnlaugur <gju@kth.se>
2016-04-27 17:55:22 +0200

    Java docs
    Java docs for TestDASL and ActiveServiceLookup

--- src/cotton/services/ActiveServiceLookup.java
+++ src/cotton/services/ActiveServiceLookup.java
@@ -1,19 +1,39 @@
 package cotton.services;
+
 import java.util.Enumeration;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- *
- *
+ * The <code>ActiveServiceLookup</code> acts as a lookup table for services. The
+ * class implementing the interface is intended to be used by the 
+ * <code>ServiceHandler</code> to register, get services as well as remove services 
+ * from the lookup table.
+ * 
  * @author Tony
  * @author Magnus
+ * @see ServiceHandler
  **/
 public interface ActiveServiceLookup {
 
+    /**
+     * Registers a service to the lookup table. To register a service the user
+     * has to define the service name, the factory used to make instances of the service
+     * and the maximum capacity of the service instances.
+     * 
+     * @param serviceName defines what the service is called.
+     * @param serviceFactory the factory used to make instances of the service.
+     * @param maxCapacity defines how many instances of the service are allowed.
+     * @return whether the registration was successful or not.
+     */
     public boolean registerService(String serviceName, ServiceFactory serviceFactory,int maxCapacity);
 
+    /**
+     * Returns the <code>ServiceMetaData</code> for a specified service.
+     * 
+     * @param serviceName the specified service.
+     * @return the meta data of the service.
+     */
     public ServiceMetaData getService(String serviceName);
-
     public Enumeration<String> getServiceEnumeration();
     public ConcurrentHashMap.KeySetView<String, ServiceMetaData> getKeySet();
 
--- src/cotton/test/TestDASL.java
+++ src/cotton/test/TestDASL.java
@@ -18,48 +18,49 @@ import java.io.Serializable;
 import java.util.Enumeration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
+ * A test class for the <code>DefaultActiveServiceLookup</code> class as well as
+ * the <code>ServiceFactory</code> interface.
+ * 
  * @author Gunnlaugur Juliusson
  */
 public class TestDASL {
 
-    public TestDASL() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
+    /**
+     * A test service factory to generate test service instances.
+     */
     public class TestServiceFactory implements ServiceFactory {
 
+        /**
+         * Returns an <code>TestServiceInstance</code> for testing purposes.
+         * 
+         * @return an TestServiceInstance.
+         * @see TestServiceInstance
+         */
         @Override
         public ServiceInstance newServiceInstance() {
             return new TestServiceInstance();
         }
 
-        //TODO Test and change
+        /**
+         * A service instance intended for testing the <code>DefaultActiveServiceLookup</code> class.
+         */
         public class TestServiceInstance implements ServiceInstance {
-
+            
+            /**
+             * Retrieves the data and converts it to an <code>int</code> and multiplies it by two.
+             * The number sent through the <code>InputStream</code> should be defined as an <code>Integer</code>.
+             * 
+             * @param ctx contains the cloud context.
+             * @param from describes who sent the original request.
+             * @param data the number to be multiplied.
+             * @param to describes where the service should redirect the request before returning the number.
+             * 
+             * @return the incoming number multiplied by 2.
+             */
             @Override
             public Serializable consumeServiceOrder(CloudContext ctx, ServiceConnection from, InputStream data, ServiceChain to) {
                 int number = convertInputStream(data);
@@ -69,15 +70,20 @@ public class TestDASL {
                 return number;
             }
 
-            //TODO Complete
+            /**
+             * Converts the data in the <code>InputStream</code> to an <code>int</code> and returns it.
+             * The data written to the <code>InputStream</code> needs to have been an <code>Integer</code> 
+             * to successfully convert.
+             * 
+             * @param data a <code>Integer</code> to convert to <code>int</code>.
+             * @return the number read from the <code>InputStream</code>.
+             */
             private int convertInputStream(InputStream data) {
                 Integer number = -1;
 
                 ObjectInputStream inStream;
                 try {
-                    System.out.println("asd");
                     inStream = new ObjectInputStream(data);
-                    System.out.println("asd");
                     number = (Integer)inStream.readObject();
 
                 } catch (IOException ex) {
@@ -91,14 +97,26 @@ public class TestDASL {
         }
     }
 
+    /**
+     * Tests the capacity implementation of the <code>DefaultActiveServiceLookup</code> class.
+     * Capacity is set to ten during registration and then compared through the 
+     * <code>getMaxCapacity()</code> function.
+     */
     @Test
     public void testCapacity() {
         ActiveServiceLookup dasl = new DefaultActiveServiceLookup();
-        dasl.registerService("Coloring", new TestServiceFactory(), 10);
+        dasl.registerService("Coloring", null, 10);
 
         assertEquals(10, dasl.getService("Coloring").getMaxCapacity());
     }
 
+    /**
+     * Tests the capacity implementation of the <code>DefaultActiveServiceLookup</code> class.
+     * Capacity is set to ten during registration and then compared to nine through the 
+     * <code>getMaxCapacity()</code> function. If the registration is successful the
+     * function will respond that the serviceFactory() does not contain a 
+     * <code>maxCapacity</code> of nine.
+     */
     @Test
     public void testWrongCapacity() {
         ActiveServiceLookup dasl = new DefaultActiveServiceLookup();
@@ -107,6 +125,11 @@ public class TestDASL {
         assertTrue(9 != maxCapacity);
     }
 
+    /**
+     * Tests the hash map keys by comparing existing values with 
+     * <code>"Multiplying"</code>. The function registers 3 services and then
+     * loops through the entire list checking for <code>"Multiplying"</code>.
+     */
     @Test
     public void testHashMapKeys() {
         ActiveServiceLookup dasl = new DefaultActiveServiceLookup();
@@ -117,7 +140,7 @@ public class TestDASL {
         services[2] = "Yellow Filtering";
 
         for(int i = 0; i < services.length; i++)
-            dasl.registerService(services[i], new TestServiceFactory(), 10);
+            dasl.registerService(services[i], null, 10);
 
         Enumeration<String> hashMapKeys = dasl.getServiceEnumeration();
 
@@ -132,6 +155,10 @@ public class TestDASL {
         assertEquals("Multiplying", temp);
     }
 
+    /**
+     * Tests the hash map keys by removing a value and checking whether its 
+     * removed or not.
+     */
     @Test
     public void testRemove() {
         ActiveServiceLookup dasl = new DefaultActiveServiceLookup();
@@ -153,6 +180,14 @@ public class TestDASL {
         assertEquals(true, removeCheck);
     }
     
+    /**
+     * Tests the <code>ServiceFactory</code> interface by implementing an
+     * <code>TestServiceFactory</code>. The test is designed to check whether the
+     * design of the interface can be used to implement a new factory as well as
+     * services.
+     * 
+     * @throws IOException thrown if the streams are unsuccessful.
+     */
     @Test
     public void testFactory() throws IOException {
         ServiceFactory sf = new TestServiceFactory();
