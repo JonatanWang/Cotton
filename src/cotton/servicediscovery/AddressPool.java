7b63fd26dbd73ec66f9cf482cc7d1b037518edf0
gunnlaugur <gju@kth.se>
2016-04-28 11:51:02 +0200

    Javadoc Comments - AddressPool
    Comments for the AddressPool class.

--- src/cotton/servicediscovery/AddressPool.java
+++ src/cotton/servicediscovery/AddressPool.java
@@ -4,14 +4,24 @@ import java.net.SocketAddress;
 import java.util.ArrayList;
 
 /**
- *
+ * The <code>AddressPool</code> acts as a list of <code>SocketAddresses</code> 
+ * that you can add and remove from. The pool is initialized with zero 
+ * <code>SocketAddresses</code>.
+ * 
  * @author Magnus, Mats
+ * @see SocketAddress
  */
-class AddressPool {
+public class AddressPool {
 
     private int pos = 0;
-    private ArrayList<SocketAddress> pool = new ArrayList<SocketAddress>();
+    private ArrayList<SocketAddress> pool = new ArrayList<>();
 
+    /**
+     * Adds a <code>SocketAddress</code> to the <code>AddressPool</code> concurrently.
+     * 
+     * @param address the <code>SocketAddress</code> to add to the <code>AddressPool</code>.
+     * @return <code>true</code> always.
+     */
     public boolean addAddress(SocketAddress address) {
         synchronized (this) {
             pool.add(address);
@@ -19,6 +29,12 @@ class AddressPool {
         return true;
     }
 
+    /**
+     * Returns the next <code>SocketAddress</code> in the <code>AddressPool</code>. 
+     * If the <code>AddressPool</code> is empty the function will reply <code>null</code>.
+     * 
+     * @return the next <code>SocketAddress</code> or null if the list is empty.
+     */
     public SocketAddress getAddress() {
         SocketAddress addr = null;
         
