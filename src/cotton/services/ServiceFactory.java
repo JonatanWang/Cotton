5c1645e9ff820faa6ae9b0fe9816d3d77de94ac3
gunnlaugur <gju@kth.se>
2016-04-28 10:07:28 +0200

    Javadoc comments
    Comments about ServiceChain, ActiveServiceLookup, ServiceFactory,
    ServiceInstance and ServiceMetaData.

--- src/cotton/services/ServiceFactory.java
+++ src/cotton/services/ServiceFactory.java
@@ -1,13 +1,20 @@
 package cotton.services;
 
 /**
- *
+ * Implementing this interface allows a class to acts as a service factory. 
+ * The <code>ServiceFactory</code> has a specific <code>ServiceInstance</code> 
+ * connected to it.
  *
  * @author Tony
  * @author Magnus
+ * @see ServiceInstance
  */
 public interface ServiceFactory {
 
+    /**
+     * Returns an instance of the service connected to the <code>ServiceFactory</code>.
+     * 
+     * @return the connected service.
+     */
     public ServiceInstance newServiceInstance();
-
 }
--- src/cotton/services/ServiceMetaData.java
+++ src/cotton/services/ServiceMetaData.java
@@ -2,42 +2,44 @@ package cotton.services;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-
 /**
- *
+ * The <code>ServiceMetaData</code> consists of information about the service. 
+ * The <code>ServiceHandler</code> needs to manually update the thread count for
+ * the <code>currentThreadCount</code> to work.
  *
  * @author Tony
  * @author Magnus
+ * @see ServiceHandler
  */
 public class ServiceMetaData {
-        private ServiceFactory serviceFactory;
-        private int maxCapacity;
-        private AtomicInteger currentThreadCount;
+    private ServiceFactory serviceFactory;
+    private int maxCapacity;
+    private AtomicInteger currentThreadCount;
 
-        public ServiceMetaData(ServiceFactory serviceFactory, int maxCapacity) {
-                this.maxCapacity = maxCapacity;
-                this.serviceFactory = serviceFactory;
-                currentThreadCount = new AtomicInteger();
-        }
+    public ServiceMetaData(ServiceFactory serviceFactory, int maxCapacity) {
+            this.maxCapacity = maxCapacity;
+            this.serviceFactory = serviceFactory;
+            currentThreadCount = new AtomicInteger();
+    }
 
-        //TODO GET BETTER NAME
-        public int serviceStarted(){
-                return currentThreadCount.incrementAndGet();
-        }
+    //TODO GET BETTER NAME
+    public int serviceStarted(){
+            return currentThreadCount.incrementAndGet();
+    }
 
-        public int serviceFinished(){
-                return currentThreadCount.decrementAndGet();
-        }
+    public int serviceFinished(){
+            return currentThreadCount.decrementAndGet();
+    }
 
-        public int getMaxCapacity() {
-                return maxCapacity;
-        }
+    public int getMaxCapacity() {
+            return maxCapacity;
+    }
 
-        public int getCurrentThreadCount(){
-                return currentThreadCount.get();
-        }
+    public int getCurrentThreadCount(){
+            return currentThreadCount.get();
+    }
 
-        public ServiceFactory getServiceFactory(){
-                return serviceFactory;
-        }
+    public ServiceFactory getServiceFactory(){
+            return serviceFactory;
+    }
 }
