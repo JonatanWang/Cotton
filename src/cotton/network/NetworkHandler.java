3f959a990344bc0537bb67d5c3694bbd7ff02f02
gunnlaugur <gju@kth.se>
2016-04-28 15:37:00 +0200

    Javadoc - the servicediscovery packet
    Comments for AnnouncePacket, DiscoveryPacket and the DiscoveryProbe.

--- src/cotton/network/NetworkHandler.java
+++ src/cotton/network/NetworkHandler.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package network;
 
 import java.net.SocketAddress;
@@ -16,5 +11,4 @@ public interface NetworkHandler extends Runnable {
     public boolean send(cotton.network.NetworkPacket netPacket, SocketAddress addr);
     public CountDownLatch sendKeepAlive(cotton.network.NetworkPacket netPacket,SocketAddress addr);
     public void stop();
-
 }
--- src/cotton/servicediscovery/DiscoveryProbe.java
+++ src/cotton/servicediscovery/DiscoveryProbe.java
@@ -1,36 +1,65 @@
-
 package cotton.servicediscovery;
 
 import java.io.Serializable;
 import java.net.SocketAddress;
 
 /**
- *
+ * The <code>DiscoveryProbe</code> acts as a discovery request as well as a
+ * discovery response. The packet consists of a service name and a 
+ * <code>SocketAddress</code> containing the targeted service address. 
+ * 
  * @author Magnus
  */
 public class DiscoveryProbe implements Serializable {
-        private String name;
-        private SocketAddress address;
+    private String name;
+    private SocketAddress address;
 
-        public DiscoveryProbe(String name, SocketAddress address) {
-            this.name = name;
-            this.address = address;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public SocketAddress getAddress() {
-            return address;
-        }
-
-        public void setAddress(SocketAddress address) {
-            this.address = address;
-        }
-        
+    /**
+     * Constructs a <code>DiscoveryPack</code> through the in parameters.
+     * 
+     * @param name the service name.
+     * @param address the service address.
+     */
+    public DiscoveryProbe(String name, SocketAddress address) {
+        this.name = name;
+        this.address = address;
     }
+
+    /**
+     * Returns the service name contained in the packet.
+     * 
+     * @return the service name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the service name in the packet.
+     * 
+     * @param name the service name.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the <code>SocketAddress</code> contained in the packet. This address
+     * points to the service location.
+     * 
+     * @return the <code>SocketAddress</code>
+     */
+    public SocketAddress getAddress() {
+        return address;
+    }
+
+    /**
+     * Sets the <code>SocketAddress</code> in the packet. This address should 
+     * point to the service location.
+     * 
+     * @param address the new address.
+     */
+    public void setAddress(SocketAddress address) {
+        this.address = address;
+    }        
+}
